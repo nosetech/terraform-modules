441dda44df921813ebdfbb1114cdf281aedd8019
能勢容幸 <system.nosetech@gmail.com>
2022-03-11 16:21:35 +0900

    Route53+ACM(SSL証明書認証) #1

---
+++ examples/route53_with_cert/outputs.tf
@@ -0,0 +1,7 @@
+output "zone_id" {
+  value = module.dns.zone_id
+}
+
+output "cert_arn" {
+  value = module.dns.cert_arn
+}
---
+++ modules/aws/route53_with_cert/variables.tf
@@ -0,0 +1,8 @@
+variable domain_name {}
+variable comment {
+    default = "Managed by Terraform"
+}
+variable hostedzone_tags {}
+
+variable cert_domain_name {}
+variable cert_tags {}
